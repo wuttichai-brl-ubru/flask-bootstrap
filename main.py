e046bc2376cfec320b357f4d30c24ab605448c26
wuttichai-brl-ubru <std.67122420309@ubru.ac.th>
2026-01-13 11:05:22 +0700

    update case-insensitive search

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def index():
 @app.route('/cars', methods=['GET', 'POST'])
 def show_cars():
   if request.method == 'POST':
-    brand = request.form['brand']
+    brand = request.form['brand'].lower()
     # print(brand)
     tmp_cars = []
     for car in cars:
       # print(car)
-      if brand in car['brand']:
+      if brand in car['brand'].lower():
         tmp_cars.append(car)
     return render_template('cars/cars.html',
                          title='Show Cars by Brand Page',
